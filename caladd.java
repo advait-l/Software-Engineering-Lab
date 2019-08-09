3e2e14c87f7040274c1b275e9494beb5c7b51d93
advait-l <advait.lonkar@iiitb.org>
2019-08-09 16:13:19 +0530

    multiply case

--- caladd.java
+++ caladd.java
@@ -19,7 +19,13 @@ import java.util.Scanner;
                                res = a + b;
                                System.out.print("Result = " + res);
                                break;
-                    
+                    case '2' : System.out.print("Enter Two numbers: ");
+                               a = scan.nextFloat();
+                               b = scan.nextFloat();
+                               res = a * b;
+                               System.out.print("Result = " + res);
+                               break;
+
                     case '3' : System.exit(0);
                                break;
                     default : System.out.print("INVALID");
